12ec4805899ae8974c6a46303ccea47de16de57a
Dan McArdle <zingermc@gmail.com>
2014-01-16 17:33:43 -0500

    infix operators

--- scratch.hs
+++ scratch.hs
@@ -13,3 +13,27 @@ quicksort (p:xs) = (quicksort smaller) ++ [p] ++ (quicksort bigger)
           bigger = filter (> p) xs 
 
 main = print $ quicksort [1,2,5,3,1,56,3,2]
+
+{-
+        DIY Operators
+
+Haskell allows the user to define infix operators by hand.
+
+Operator names are built from 
+    ! # $ % & * + . / < = > ? \ ^ | : - ~
+-}
+
+x ^^^ y = (x*y)^(x+y)
+
+
+{-
+        Practicing working with Int
+-}
+threeAvg :: Int -> Int -> Int -> Float
+threeAvg x y z = fromIntegral(x+y+z) / 3.0
+
+numGreaterThanAvg :: Int -> Int -> Int -> Int
+numGreaterThanAvg x y z = length bigUns
+                  where avg = threeAvg x y z                    -- Float
+                        floats = map (fromIntegral) [x,y,z]     -- [Float]
+                        bigUns = filter (> avg) floats
